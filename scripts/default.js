fbddc8eb3dcf27e45747b563b6c8d43185dbc898
Gaurav Mehla <gauravmehla@Gauravs-MacBook-Air.local>
2022-04-08 13:03:18 -0700

    about form added

--- scripts/default.js
+++ scripts/default.js
@@ -1,5 +1,5 @@
 // Load page after click
-function loadPage(href) {
+let loadPage = (href) => {
     // Scroll to the top
     document.getElementById('content').scroll({top:0,behavior:'smooth'});
 
@@ -15,4 +15,25 @@ function loadPage(href) {
 window.onload = () => {
     // Default page to load
     loadPage('history');
+}
+
+let submit = () => {
+    let fname = document.getElementById("fname").value;
+    let lname = document.getElementById("lname").value;
+    let country = document.getElementById("country").value;
+    let say = document.getElementById("say").value;
+
+    if(!fname) {
+        document.getElementById("fname").focus();
+        return false;
+    } else if (!lname) {
+        document.getElementById("lname").focus();
+        return false;
+    } else if(!say) {
+        document.getElementById("say").focus();
+        return false;
+    }
+
+    alert(`Hey ${fname} ${lname}, Thanks for filling the form. We will get back to you soon. Have a good day.`);
+    return true;
 }
